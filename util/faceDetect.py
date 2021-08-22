63198aa76c5992c6a2f008887c6bec27768a67ad
Bee <bee.wilkerson@ymail.com>
2021-08-22 11:28:09 -0700

    added option to utils/faceDetect.py to use imutils.video or picamera
    I wanted to test and see if using picamera package or imutils.video was faster at capturing images and improved the FPS of face detection.  Conclusion:  At the same resolution, imutils.video or picamera had no effect on overall FPS

--- util/faceDetect.py
+++ util/faceDetect.py
@@ -1,13 +1,33 @@
 
 
 from imutils.video import VideoStream
-import imutils
 import face_recognition
 import cv2
 import time
+import picamera
+import argparse
+from picamera.array import PiRGBArray
 
-vs = VideoStream(usePiCamera=True).start()
-time.sleep(2)
+CAPTURE_WIDTH = 320
+CAPTURE_HEIGHT = 208
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--imutilsVideo',
+                    dest='useImutilsVideo',
+                    action='store_true')
+args = parser.parse_args()
+
+vs = None
+if args.useImutilsVideo:
+    print('Using imutils.video to caption image stream')
+    vs = VideoStream(usePiCamera=True).start()
+    time.sleep(2)
+else:
+    print('Using picamera package to caption image stream')
+    camera = picamera.PiCamera()
+    camera.resolution = (CAPTURE_WIDTH, CAPTURE_HEIGHT)
+    # camera.framerate = 20
+    rawCapture = PiRGBArray(camera, size=(CAPTURE_WIDTH, CAPTURE_HEIGHT))
 
 print('Face Detection running')
 print('(press ctrl+C to stop)')
@@ -15,26 +35,37 @@ print('(press ctrl+C to stop)')
 frameCount = 0
 startTime = time.time()
 
+
+def process_frame(frameImage):
+    global frameCount
+    frameCount += 1
+
+    # Find all the faces in the current frame of video
+    face_locations = face_recognition.face_locations(frameImage)
+
+    # Display the results
+    for top, right, bottom, left in face_locations:
+        # Draw a box around the face
+        cv2.rectangle(frameImage, (left, top),
+                      (right, bottom), (0, 0, 255), 2)
+
+    # Display the resulting image
+    cv2.imshow('Face detection running', frameImage)
+
+    # TODO: why is this necessary?  Without it, imshow doesn't show anything
+    cv2.waitKey(1)
+
+
 try:
-    while True:
-        frame = vs.read()
-        # frame = imutils.resize(frame, width=500)
-        frameCount += 1
+    if args.useImutilsVideo:
+        while True:
+            frame = vs.read()
+            process_frame(frame)
+    else:
+        for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+            process_frame(frame.array)
+            rawCapture.truncate(0)
 
-        # Find all the faces in the current frame of video
-        face_locations = face_recognition.face_locations(frame)
-
-        # Display the results
-        for top, right, bottom, left in face_locations:
-            # Draw a box around the face
-            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-
-        # Display the resulting image
-        cv2.imshow('Face detection running', frame)
-
-        # TODO: why is this necessary?  Without it, imshow doesn't show anything
-        if cv2.waitKey(1) == 13:
-            break
 
 except KeyboardInterrupt:
     print("")
@@ -42,7 +73,7 @@ except KeyboardInterrupt:
 
 totalTime = time.time() - startTime
 
-vs.stop()
+# vs.stop()
 cv2.destroyAllWindows()
 
 
