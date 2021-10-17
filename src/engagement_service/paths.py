b353bd75b52c0b7e49663ad67d418a3f18b6f74a
Bee <bee.wilkerson@ymail.com>
2021-10-16 17:56:10 -0700

    WIP: start of breaking out face rec to its own service

---
+++ src/engagement_service/paths.py
@@ -0,0 +1,10 @@
+
+
+TMP_DATA_DIR = "data/engagement"
+TMP_RAW_NAME_FILE = f"{TMP_DATA_DIR}/raw-name.wav"
+TMP_NAME_FILE = f"{TMP_DATA_DIR}/name.wav"
+
+FACES_DATA_DIR = "data/faces"
+
+ENCODINGS_FILE_PATH = "data/encodings.pickle"
+TRAINER_PROCESSED_FILE_PATH = "data/faces_processed.json"
---
+++ src/engagement_service/server.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""
+ This was originally pilfered from
+ https://github.com/adeept/Adeept_RaspTank/blob/a6c45e8cc7df620ad8977845eda2b839647d5a83/server/app.py
+
+ Which looks like it was in turn pilfered from
+ https://blog.miguelgrinberg.com/post/flask-video-streaming-revisited
+
+"Great artists steal". Thank you, @adeept and @miguelgrinberg!
+"""
+
+import os
+import threading
+import psutil
+
+from flask import Flask, Response, send_from_directory
+from flask_cors import CORS
+
+import cv2
+from camera_opencv import Camera
+from base_camera import BaseCamera
+from face_detect import FaceDetect
+from trainer import Trainer
+from head import Head
+from engagement import Engagement
+
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+camera = Camera()
+face_detect = FaceDetect(camera)
+trainer = Trainer()
+head = Head()
+# this starts a thread that engages with the huuuumans
+engagement = Engagement(camera, face_detect, trainer, head)
+
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        frame = frame.copy()
+        frame = face_detect.augment_frame(frame)
+        frame = engagement.augment_frame(frame)
+        jpeg = cv2.imencode('.jpg', frame)[1].tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + jpeg + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(camera),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+@app.route('/stats')
+def send_stats():
+    return {
+        "capture": BaseCamera.stats(),
+        "engagement": Engagement.stats(),
+        "faceDetect": FaceDetect.stats(),
+        "system": {
+            "cpuPercent": psutil.cpu_percent(),
+            "ram": psutil.virtual_memory()[2],
+        },
+        "trainer": Trainer.stats()
+
+    }
+
+
+@app.route('/pauseEngagement')
+def pause_engagement():
+    Engagement.pause_engagement()
+    return {"status": "paused"}
+
+
+@app.route('/resumeEngagement')
+def resume_engagement():
+    Engagement.resume_engagement()
+    return {"status": "resumed"}
+
+
+@app.route('/centerHead')
+def center_head():
+    head.center_head()
+
+
+@app.route('/<path:filename>')
+def sendgen(filename):
+    return send_from_directory(dir_path, filename)
+
+
+@app.route('/')
+def index():
+    return send_from_directory(dir_path, 'index.html')
+
+
+class webapp:
+    def __init__(self):
+        self.camera = camera
+
+    def thread(self):
+        app.run(host='0.0.0.0', threaded=True)
+
+    def startthread(self):
+        # Define a thread for FPV and OpenCV
+        fps_threading = threading.Thread(target=self.thread)
+        # 'True' means it is a front thread,it would close when the mainloop() closes
+        fps_threading.setDaemon(False)
+        fps_threading.start()  # Thread starts
+
+
+flask_app = webapp()
+flask_app.startthread()
